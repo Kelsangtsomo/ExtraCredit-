a41dcdd90f2222dbf9a74c81688782661aad8985
Kelsangg <58080314+Kelsangtsomo@users.noreply.github.com>
2020-12-15 20:48:07 -0500

    hI ITS ME

---
+++ Bioo/src/bio/Bio.java
@@ -0,0 +1,12 @@
+package bio;
+
+public class Bio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//please let us finish this respository business alreayd 
+		
+	}
+
+}
