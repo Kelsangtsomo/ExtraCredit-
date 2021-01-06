115eb3cc6673f05ac5e059ec97f05c4a209545f1
Owner <Owner@192.168.2.17>
2021-01-05 19:35:26 -0500

    j

--- Bioo/src/bio/Bio.java
+++ Bioo/src/bio/Bio.java
@@ -6,8 +6,8 @@ public class Bio {
 	public static void main(String[] args) {
 		
 		Console c = new Console();
-		c.println("Welcome to Alpha Fold game. Would you like to play?");
-		c.print("Please enter your name: ");
+		System.out.println("Welcome to Alpha Fold game. Would you like to play?");
+		System.out.println("Please enter your name: ");
 		String name = c.readLine();
  		
 	}
