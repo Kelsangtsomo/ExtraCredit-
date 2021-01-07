8eb185cdf0fe25198302098c9fd0bbcdff2d45e1
sanja <sanja@192.168.2.13>
2021-01-06 21:51:53 -0500

    hsa check

--- Bioo/src/bio/Bio.java
+++ Bioo/src/bio/Bio.java
@@ -1,12 +1,13 @@
 package bio;
+import hsa_new.Console;
 public class Bio {
-	
+	public static Console c = new Console (32,160);
 	public static void main(String[] args) {
 		
 		
 
-		System.out.println("Welcome to Alpha Fold game. Would you like to play?");
-		System.out.println("Please enter your name: ");
+		c.println("Welcome to Alpha Fold game. Would you like to play?");
+		c.println("Please enter your name: ");
 		
 		
 	}
