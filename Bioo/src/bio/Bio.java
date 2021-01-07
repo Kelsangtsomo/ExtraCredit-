67d0fc5dbdfe436cc1828e782d8b47ae901e26a9
sanja <sanja@192.168.2.13>
2021-01-06 20:25:50 -0500

    ignore this im just testing something

--- Bioo/src/bio/Bio.java
+++ Bioo/src/bio/Bio.java
@@ -1,21 +1,14 @@
 package bio;
-import hsa_new.Console;
-
 public class Bio {
-
+	
 	public static void main(String[] args) {
 		
-		Console c = new Console();
+		
 
 		System.out.println("Welcome to Alpha Fold game. Would you like to play?");
 		System.out.println("Please enter your name: ");
 		
-		c.println("Welcome to Alpha Fold game. Would you like to play?");
-
-		c.println("Please enter your name.");
 		
-		care bears
-
 	}
 
 }
